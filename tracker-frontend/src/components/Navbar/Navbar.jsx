48ec8be6915a5055b8cada29215f6ba304641a2c
Allan <matrixboston7613@gmail.com>
2021-12-23 19:37:43 +0800

    Basic Navbar complete, yet to add styling

---
+++ tracker-frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./Navbar.module.scss";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>TICKET TRACKER</h1>
+    </nav>
+  )
+}
+
+export default Navbar
